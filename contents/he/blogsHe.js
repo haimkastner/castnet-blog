d17e1e86dda4a71be84d08037ad76f6717857b6c
haim kastner <haim.kastner@gmail.com>
2019-09-07 23:27:50 +0300

    environments variables artical #3

--- contents/he/blogsHe.js
+++ contents/he/blogsHe.js
@@ -1,4 +1,5 @@
 export default [
+  'environments-variables-and-frontend',
   'flashing-tasmota-on-tuya-ir-bridge',
   'earth-coordinates-and-spatial-data',
   'dummy-recursion-in-js-and-its-solution-with-async-await',
